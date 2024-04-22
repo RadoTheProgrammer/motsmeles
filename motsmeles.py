d300ae1ea5edee5f1cfbf61864a652d6406e283e
RadoTheProgrammer <rado@arazakar.com>
2024-04-22 18:06:42 +0200

    Revert "move func _generate()"
    This reverts commit 864086214ef97aa64f85abe2d9223db6b81290b8.

--- motsmeles.py
+++ motsmeles.py
@@ -50,7 +50,105 @@ class Grid:
         grid = np.empty((height, width), dtype=str)
         answers = pd.DataFrame(columns=['word', 'direction', 'x1', 'y1','x2', 'y2'])
         
+        def _generate():
+            # 1. Placer les mots
+            for word in words:
+                #Vérification du mot
+                word = word.upper()
+                for letter in word:
+                    assert letter in ALPHABET,f"Mot invalid: {word}"
+                dim = min(width, height)
+                assert len(word) <= dim, f"Mot trop long: {word}"
+                
+                #choix de rangey, rangex, addy, addx
+                addx,addy = random.choice(tuple(possible_directions))
+                if addy==1:
+                    rangey=range((height-len(word))+1)
+                    
+                elif addy==-1:
+                    rangey=range(len(word)-1,height)
 
+                else:
+                    rangey=range(height)
+
+                if addx==1:
+                    rangex=range((width-len(word))+1)
+
+                elif addx==-1:
+                    rangex=range(len(word)-1,width)
+
+                else:
+                    rangex=range(width)
+                    addx=0
+
+                choixcoordonnees=[]
+                for y1 in rangey:
+
+                    for x1 in rangex:
+
+                        x2,y2=x1,y1 #create copy of x,y
+                        Break=False
+                        for letter in word:
+
+                            #Verify if it's a good emplacement mean: verify if it's blank OR filled with the right letter
+                            if grid[y2,x2]!="":
+                                if grid[y2,x2]!=letter:
+                                    break
+                            x2+=addx
+                            y2+=addy
+                        else:               
+                            choixcoordonnees.append((y1,x1))
+                if not choixcoordonnees: 
+                    
+                    raise GenerateError(f"Unable to place the word {word}")
+                y1,x1=random.choice(choixcoordonnees)
+                #xh,yh=coordonnees(index)
+                x2,y2=x1,y1
+                for letter in word:
+                    grid[y2,x2]=letter # a big debug: replace with yh, xh
+
+                    x2+=addx
+                    y2+=addy
+                answers.loc[len(answers)] = {'word': word, 'direction': (addx,addy), 'x1': x1, 'y1': y1,'x2': x2, 'y2': y2}
+
+            def verifsens(addx,addy):
+                """Verify if the letter can be placed without placing a word in the same direction"""
+                grid[y1,x1]=letter 
+                word=letter
+                xh,yh=x1,y1
+                while 0<=xh<width and 0<=yh<height:
+                    if grid[yh,xh]=="":
+                        return True
+                    if word in words:
+                        return False
+                    #lettrespossible.append(letter)
+                    word+=grid[yh,xh]
+                    xh+=addx
+                    yh+=addy 
+                return True
+
+            for y1 in range(height):
+                for x1 in range(width):
+                    for _ in range(1):
+                        xs,ys=x1,y1
+                        lettrespossible=[]
+                        if grid[y1,x1]:
+                            #print(repr(letters[y,x]))
+                            break
+                        for letter in ALPHABET:
+                            for addx,addy in [(0,1),(1,1),(1,0),(1,-1),(0,-1),(-1,-1),(-1,0),(-1,1)]:
+                                if not verifsens(addx,addy):
+                                    #lettrespossible.append(letter)
+                                    break
+                            else:
+                                lettrespossible.append(letter)
+                        if not lettrespossible:
+                            raise GenerateError(f"Unable to place {x1},{y1}")
+                        grid[y1,x1]=random.choice(lettrespossible)
+                    if grid[ys,xs] not in ALPHABET:
+                        pass
+                
+            return cls(grid,words),answers
         errs=[]
         for _ in range(10):
             try:
@@ -60,105 +158,7 @@ class Grid:
                     print(err)
         else:
             raise GenerateError("Unable to generate a grid")
-    def _generate():
-        # 1. Placer les mots
-        for word in words:
-            #Vérification du mot
-            word = word.upper()
-            for letter in word:
-                assert letter in ALPHABET,f"Mot invalid: {word}"
-            dim = min(width, height)
-            assert len(word) <= dim, f"Mot trop long: {word}"
-            
-            #choix de rangey, rangex, addy, addx
-            addx,addy = random.choice(tuple(possible_directions))
-            if addy==1:
-                rangey=range((height-len(word))+1)
-                
-            elif addy==-1:
-                rangey=range(len(word)-1,height)
-
-            else:
-                rangey=range(height)
-
-            if addx==1:
-                rangex=range((width-len(word))+1)
-
-            elif addx==-1:
-                rangex=range(len(word)-1,width)
-
-            else:
-                rangex=range(width)
-                addx=0
-
-            choixcoordonnees=[]
-            for y1 in rangey:
-
-                for x1 in rangex:
-
-                    x2,y2=x1,y1 #create copy of x,y
-                    Break=False
-                    for letter in word:
-
-                        #Verify if it's a good emplacement mean: verify if it's blank OR filled with the right letter
-                        if grid[y2,x2]!="":
-                            if grid[y2,x2]!=letter:
-                                break
-                        x2+=addx
-                        y2+=addy
-                    else:               
-                        choixcoordonnees.append((y1,x1))
-            if not choixcoordonnees: 
-                
-                raise GenerateError(f"Unable to place the word {word}")
-            y1,x1=random.choice(choixcoordonnees)
-            #xh,yh=coordonnees(index)
-            x2,y2=x1,y1
-            for letter in word:
-                grid[y2,x2]=letter # a big debug: replace with yh, xh
-
-                x2+=addx
-                y2+=addy
-            answers.loc[len(answers)] = {'word': word, 'direction': (addx,addy), 'x1': x1, 'y1': y1,'x2': x2, 'y2': y2}
-
-        def verifsens(addx,addy):
-            """Verify if the letter can be placed without placing a word in the same direction"""
-            grid[y1,x1]=letter 
-            word=letter
-            xh,yh=x1,y1
-            while 0<=xh<width and 0<=yh<height:
-                if grid[yh,xh]=="":
-                    return True
-                if word in words:
-                    return False
-                #lettrespossible.append(letter)
-                word+=grid[yh,xh]
-                xh+=addx
-                yh+=addy 
-            return True
-
-        for y1 in range(height):
-            for x1 in range(width):
-                for _ in range(1):
-                    xs,ys=x1,y1
-                    lettrespossible=[]
-                    if grid[y1,x1]:
-                        #print(repr(letters[y,x]))
-                        break
-                    for letter in ALPHABET:
-                        for addx,addy in [(0,1),(1,1),(1,0),(1,-1),(0,-1),(-1,-1),(-1,0),(-1,1)]:
-                            if not verifsens(addx,addy):
-                                #lettrespossible.append(letter)
-                                break
-                        else:
-                            lettrespossible.append(letter)
-                    if not lettrespossible:
-                        raise GenerateError(f"Unable to place {x1},{y1}")
-                    grid[y1,x1]=random.choice(lettrespossible)
-                if grid[ys,xs] not in ALPHABET:
-                    pass
-            
-        return cls(grid,words),answers
+        
     def solve(grid,words):
         answers = pd.DataFrame(columns=['word', 'direction', 'x1', 'y1','x2', 'y2'])
         for y1 in range(grid.shape[0]):
