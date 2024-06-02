0e070fe0ddb2b054b4c8ec6b2a470afb2b83bbf4
RadoTheProgrammer <rado@arazakar.com>
2024-06-02 18:32:28 +0200

    words displayed in screen

--- motsmeles/gui.py
+++ motsmeles/gui.py
@@ -6,12 +6,13 @@ class GUI:
     screen_size = (1280, 720)
     screen_color = "black"
     letter_color = "white"
+    word_color = "white"
     grid_proportion = 3/4
     font_name = pygame.font.get_default_font()  # Get the default system font
 
     # font = pygame.font.SysFont("mishafi.ttf", 36)
-    font = pygame.font.SysFont(font_name, 36)
-    font.set_bold(True)
+    letter_font = pygame.font.SysFont(font_name, 36)
+    letter_font.set_bold(True)
 
     def __init__(self, game):
         self.game = game
@@ -23,6 +24,11 @@ class GUI:
         self.case_size = min((
             self.grid_pwidth/self.game.grid.shape[1],
             self.grid_pheight / self.game.grid.shape[0]))
+        self.word_pwidth, self.word_pheight = (
+            self.screen_size[0]*(1/self.grid_proportion),
+            self.screen_size[1]/len(self.game.words)
+        )
+        #self.word_pheight = self.words_pheight / len(self.game.words)
         # print(pygame.font.get_fonts())
         # widths,heights=[],[]
         # for letter in "ABCDEFGHIJKLMNOPQRSTUVWXYZ":
@@ -35,19 +41,46 @@ class GUI:
         # print(self.letter
         return
 
-    def get_font(self):
-        font_size = 1
+    def get_letter_font(self):
+        letter_font_size = 1
         while True:
-            font = pygame.font.Font(size=font_size)
+            font = pygame.font.Font(size=letter_font_size)
             text = font.render("A", True, self.letter_color)
             if max(text.get_size()) > self.case_size:
                 return
-            font_size += 1
-            self.font = font
+            letter_font_size += 1
+            self.letter_font = font
 
+    def get_words_font(self):
+        word=self.get_biggest_word()
+        word_font_size=1
+        while True:
+            font = pygame.font.Font(size=word_font_size)
+            text = font.render(word, True, self.word_color)
+            width,height = text.get_width(), text.get_height()
+            if width>self.word_pwidth or height>self.word_pheight:
+                print(word_font_size)
+                return
+            word_font_size += 1
+            self.word_font = font
+            
+    def get_biggest_word(self):
+        biggest_width=0
+        for word in self.game.words:
+            font = pygame.font.Font(size=12)
+            text = font.render(word,True,self.word_color)
+            width=text.get_width()
+            if width>biggest_width:
+                biggest_width=width
+                biggest_word=word
+        return biggest_word
+        
+        
+            
     def start(self):
         self.get_case_size()
-        self.get_font()
+        self.get_letter_font()
+        self.get_words_font()
         self.screen = pygame.display.set_mode(self.screen_size)
         self.screen.fill(self.screen_color)
         self.grid_lheight, self.grid_lwidth = self.game.grid.shape
@@ -74,14 +107,22 @@ class GUI:
             for row in self.game.grid:
                 x = self.case_size//2
                 for letter in row:
-                    letter_surface = self.font.render(
+                    letter_surface = self.letter_font.render(
                         letter, True, self.letter_color)
 
                     letter_rect = letter_surface.get_rect(center=(x, y))
                     self.screen.blit(letter_surface, letter_rect)
                     x += self.case_size
                 y += self.case_size
-
+            y = 0
+            for word in self.game.words:
+                #print(word)
+                x = self.grid_pwidth
+                word_surface = self.word_font.render(word,True,self.word_color)
+                self.screen.blit(word_surface, word_surface.get_rect(topleft=(int(x),int(y))))
+                #pygame.display.flip()
+                y += self.word_pheight
+            
             # RENDER YOUR GAME HERE
 
             # flip() the display to put your work on screen
